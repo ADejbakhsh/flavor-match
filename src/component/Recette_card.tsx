caf71c6a1132c21a179a936219061684d588f158
Arman Dejbakhsh <arman@dejbakhsh.fr>
2023-02-10 17:43:09 +0100

    sauvegarde du css
    Il manque la pizza sur ce commit. Celle-ci n'étant pas libre de droit.
    A vous de trouvez un jpg

--- src/component/Recette_card.tsx
+++ src/component/Recette_card.tsx
@@ -1,13 +1,22 @@
 import React from "react"
 import "../css/Recette_card.css"
-
+import photo from "../toDelete/pizza.jpg"
+import Resume_recette from "./Resume_recipe"
 
 
 function Recette_card() {
+    // photo temps prix matériel nom
+    const recipe_name = "Pizaaa avec un nom hyper long"
+    const time = 10
+    const price = 2
+    const equipement = ["Four", "Four"] // on vois pour la langue en back
 
     return (
         <div className="placeholder">
-
+            <img src={photo} />
+            <div className="bottom-left">
+                <Resume_recette recipe_name={recipe_name} time={time} price={price} equipement={equipement} />
+            </div>
         </div>
     )
 }
---
+++ src/component/Resume_recipe.tsx
@@ -0,0 +1,38 @@
+import React from "react"
+import "../css/Recette_card.css"
+
+interface resume_recipe_t {
+    recipe_name: string,
+    time: number,
+    price: number,
+    equipement: string[]
+}
+
+
+function Resume_recipe({ recipe_name, time, price, equipement }: resume_recipe_t) {
+
+    // Change le int prix en emoji
+    let string_price = ""
+    for (let i = 0; i < price; i++) {
+        string_price += "💰"
+    }
+
+    // TODO a voir avec le back si ca ca rest
+    let list_equipement = ""
+    for (let i = 0; i < equipement.length; i++) {
+        list_equipement += equipement[i]
+        if (i + 1 !== equipement.length)
+            list_equipement += ", "
+    }
+
+    return (
+        <>
+            <div className="card-title card-text">{recipe_name}</div>
+            <div className="card-text">{time} min </div>
+            <div className="card-text">{string_price}</div>
+            <div className="card-text">{list_equipement}</div>
+        </>
+    )
+}
+
+export default Resume_recipe
