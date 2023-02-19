eda9dac3b0ba968a776674a88f88118fd20c963a
Dejbakhsh <armn@dejbakhsh.fr>
2023-02-19 17:44:55 +0100

    footer css ☢ c'est moche

--- src/component/App.tsx
+++ src/component/App.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import "../css/App.css"
 import Header from "./Header"
 import Recette_card from "./Recette_card"
+import Footer from "./Footer"
 
 function App() {
     return (
@@ -10,11 +11,13 @@ function App() {
                 <Header />
             </div>
             <div className="recipe">
+                {/*TODO wrap  dans un truc qui permet de swip */}
                 <Recette_card />
                 <div />
             </div>
             <div className="footer">
-                
+                <Footer />
+                {/* accepter ou refuser la recette actuelle + commentaire quand swip vers le haut*/}
             </div>
         </div>
     )
---
+++ src/component/Footer.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import "../css/Footer.css"
+
+function Footer() {
+    // returne ❌  ✅
+    return (
+        <>
+            <div className="button">❌</div>
+            <div className="button">💬</div>
+            <div className="button">💛</div>
+        </>
+    )
+}
+
+export default Footer
